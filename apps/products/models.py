de74c16f6149893bf68a8747b66768e4ea735fad
meeklife <forsonbaah@gmail.com>
2024-05-21 09:28:25 +0100

    feat(productcategory): update description to allow blank

--- apps/products/models.py
+++ apps/products/models.py
@@ -5,7 +5,10 @@ from apps.common import models as base_models
 
 class ProductCategory(base_models.BaseModel):
     name = models.CharField(max_length=50)
-    description = models.TextField()
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ("created_at",)
 
     def __str__(self):
         return self.name
