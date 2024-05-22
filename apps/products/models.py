c4d7aec4ad0c75337a801bd9e0258732d6250a40
meeklife <forsonbaah@gmail.com>
2024-05-22 11:48:03 +0100

    feat(products): add Product model and link to Brand and Category

--- apps/products/models.py
+++ apps/products/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from apps.common import models as base_models
 
+from ..brands.models import Brand
+
 
 class ProductCategory(base_models.BaseModel):
     name = models.CharField(max_length=50)
@@ -12,3 +14,12 @@ class ProductCategory(base_models.BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Products(base_models.BaseModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    specification = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,9 +1,32 @@
 from rest_framework import serializers
 
-from .models import ProductCategory
+from .models import ProductCategory, Products
 
 
 class ProductCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductCategory
         fields = ["id", "name", "description"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    brand = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Products
+        fields = [
+            "id",
+            "name",
+            "description",
+            "price",
+            "specification",
+            "brand",
+            "category",
+        ]
+
+    def get_brand(self, product) -> str:
+        return product.brand.name if product.brand else ""
+
+    def get_category(self, product):
+        return product.category.name if product.category else ""
