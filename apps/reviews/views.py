f288b3708895df0c12b76fced9b8f24174b1b16d
meeklife <forsonbaah@gmail.com>
2024-05-24 23:04:14 +0100

    feat(reviews): add product_review views

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,3 +1,18 @@
-# from django.shortcuts import render
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import ProductReview
+from .serializers import ProductReviewSerializer
+
+
+class ProductReviewView(ModelViewSet):
+    queryset = ProductReview.objects.all()
+    serializer_class = ProductReviewSerializer
+    permission_classes = [AllowAny]
+
+    http_method_names = [m for m in ModelViewSet.http_method_names if m not in ["put"]]
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
--- config/api_urls.py
+++ config/api_urls.py
@@ -5,5 +5,6 @@ app_name = "api"
 urlpatterns = [
     path("brands/", include("apps.brands.urls")),
     path("products/", include("apps.products.urls")),
+    path("", include("apps.reviews.urls")),
     path("", include("apps.users.urls")),
 ]
