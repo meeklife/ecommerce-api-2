bc3bf1bf66ba6a3b83453b48614300e6b780f519
meeklife <forsonbaah@gmail.com>
2024-05-28 13:24:59 +0100

    fix(review): add validator to rating field range in ProductReview

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from apps.common import models as base_models
@@ -9,5 +10,7 @@ from ..users.models import User
 class ProductReview(base_models.BaseModel):
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField()
+    rating = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
     description = models.TextField(blank=True)
