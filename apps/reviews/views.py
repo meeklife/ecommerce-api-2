45cb55a788cd6da2021dfdb520e4057e137b7812
meeklife <forsonbaah@gmail.com>
2024-06-02 10:12:43 +0100

    fix(review): debugging views

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import AppReview, ProductReview
@@ -8,24 +8,28 @@ from .serializers import AppReviewSerializer, ProductReviewSerializer
 class ProductReviewView(ModelViewSet):
     queryset = ProductReview.objects.all()
     serializer_class = ProductReviewSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
-    http_method_names = [m for m in ModelViewSet.http_method_names if m not in ["put"]]
+    http_method_names = [
+        m for m in ModelViewSet.http_method_names if m not in ["put", "patch"]
+    ]
 
-    def get_permissions(self):
-        if self.action in ["create", "update", "partial_update", "destroy"]:
-            self.permission_classes = [IsAuthenticated]
-        return super().get_permissions()
+    # def get_permissions(self):
+    #     if self.action in ["create", "update", "partial_update", "destroy"]:
+    #         self.permission_classes = [IsAuthenticated]
+    #     return super().get_permissions()
 
 
 class AppReviewView(ModelViewSet):
     queryset = AppReview.objects.all()
     serializer_class = AppReviewSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
-    http_method_names = [m for m in ModelViewSet.http_method_names if m not in ["put"]]
+    http_method_names = [
+        m for m in ModelViewSet.http_method_names if m not in ["put", "patch"]
+    ]
 
-    def get_permissions(self):
-        if self.action in ["create", "update", "partial_update", "destroy"]:
-            self.permission_classes = [IsAuthenticated]
-        return super().get_permissions()
+    # def get_permissions(self):
+    #     if self.action in ["create", "update", "partial_update", "destroy"]:
+    #         self.permission_classes = [IsAuthenticated]
+    #     return super().get_permissions()
