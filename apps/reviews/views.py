cbe4af89ad3e4340d97df11ca41b3efc6b14ad22
meeklife <forsonbaah@gmail.com>
2024-06-04 22:48:37 +0100

    fix(review): fix permissions for users and authenticated users

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,4 +1,5 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import AppReview, ProductReview
@@ -8,7 +9,7 @@ from .serializers import AppReviewSerializer, ProductReviewSerializer
 class ProductReviewView(ModelViewSet):
     queryset = ProductReview.objects.all()
     serializer_class = ProductReviewSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     http_method_names = [
         m for m in ModelViewSet.http_method_names if m not in ["put", "patch"]
@@ -16,12 +17,29 @@ class ProductReviewView(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        review = self.get_object()
+        if review.user != request.user:
+            raise PermissionDenied("You do not have permission to delete this review.")
+        return super().destroy(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        review = self.get_object()
+        if review.user != request.user:
+            raise PermissionDenied("You do not have permission to update this review.")
+        return super().partial_update(request, *args, **kwargs)
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
 
 
 class AppReviewView(ModelViewSet):
     queryset = AppReview.objects.all()
     serializer_class = AppReviewSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     http_method_names = [
         m for m in ModelViewSet.http_method_names if m not in ["put", "patch"]
@@ -29,3 +47,20 @@ class AppReviewView(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        review = self.get_object()
+        if review.user != request.user:
+            raise PermissionDenied("You do not have permission to update this review.")
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        review = self.get_object()
+        if review.user != request.user:
+            raise PermissionDenied("You do not have permission to delete this review.")
+        return super().destroy(request, *args, **kwargs)
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
