a806d3598580a2256c38dba59343c23ff9243176
meeklife <forsonbaah@gmail.com>
2024-06-04 22:57:53 +0100

    refactor(orders): endpoints for read, list and update

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -8,14 +8,19 @@ class Orders(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-    http_method_names = [m for m in ModelViewSet.http_method_names if m not in ["put"]]
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    http_method_names = [
+        m
+        for m in ModelViewSet.http_method_names
+        if m not in ["put", "create", "destroy"]
+    ]
 
 
 class OrderItems(ModelViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
 
-    http_method_names = [m for m in ModelViewSet.http_method_names if m not in ["put"]]
+    http_method_names = [
+        m
+        for m in ModelViewSet.http_method_names
+        if m not in ["put", "create", "destroy"]
+    ]
