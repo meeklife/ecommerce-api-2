c95ef12f76ede47f7798887f7ba7ac38f5743828
miky-rola <mikyrola8@gmail.com>
2024-06-06 15:33:42 +0000

    Modified the invite

--- apps/invite/serializers.py
+++ apps/invite/serializers.py
@@ -3,6 +3,8 @@ from .models import Invitation
 from apps.users.models import User 
 
 class InvitationSerializer(serializers.ModelSerializer):
+    # A read-only field to represent the name of the inviter
+    # This will include the name of the inviter in the serialized representation of the Invitation object.
     user = serializers.ReadOnlyField(source="inviter.name")
 
     class Meta:
@@ -18,7 +20,7 @@ class InvitationSerializer(serializers.ModelSerializer):
     def validate_email(self, value):
         # Check if the email is already registered or an invitation is pending
         if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("This email is already registered on the platform.")
+            raise serializers.ValidationError("This email is already registered on the platform.") # used the validationError from Serializers
         elif Invitation.objects.filter(email=value, join=False).exists():
             raise serializers.ValidationError("An invitation has already been sent to this email.")
         return value
