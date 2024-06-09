2fce349fd1a81e0a3d1e23af1be0a04f01ee7614
meeklife <forsonbaah@gmail.com>
2024-06-09 14:06:29 +0100

    refactor(common): import env from base

--- apps/common/sendEmails.py
+++ apps/common/sendEmails.py
@@ -1,12 +1,5 @@
-from pathlib import Path
-
-import environ
 from django.core.mail import send_mail
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-APPS_DIR = BASE_DIR / "apps"
-env = environ.Env()
-env.read_env(str(BASE_DIR / ".env"))
+from config.settings.base import env
 
 
 def send_email(subject, message, recipient, fail_silently=False):
