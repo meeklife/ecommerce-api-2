fa87724e46415a4bec05928db93d338441570b5a
meeklife <forsonbaah@gmail.com>
2024-07-10 15:07:05 +0100

    feat(order): add payment endpoint

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -4,8 +4,6 @@ from apps.orders.views import OrderViewSet
 
 router = DefaultRouter()
 
-
-# router.register("item", OrderItems, basename="order_item")
 router.register("", OrderViewSet, basename="order")
 
 urlpatterns = router.urls
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -3,12 +3,14 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from django.db import transaction
 
 from apps.cart.models import CartItem, ShoppingCart
 from apps.finance.models import Transaction
 from apps.orders.models import Order, OrderItem
 from apps.orders.serializers import OrderItemSerializer, OrderSerializer
 from apps.users.models import Address
+from apps.inventory.models import Inventory
 
 
 class OrderViewSet(ModelViewSet):
@@ -25,22 +27,24 @@ class OrderViewSet(ModelViewSet):
         return Order.objects.none()
 
     @action(detail=False, methods=["post"], url_path="checkout")
+    @transaction.atomic
     def checkout(self, request):
         cart = ShoppingCart.objects.filter(user=request.user).first()
         cart_items = CartItem.objects.filter(cart=cart.id)
+        if not cart_items:
+            return Response({"detail": "Cart is empty"}, status=status.HTTP_400_BAD_REQUEST)
 
         total_amount = sum(item.product.price * item.quantity for item in cart_items)
         delivery_cost = 10
 
         user_address = Address.objects.filter(user=request.user).first()
-        transaction = Transaction.objects.filter(user=request.user).first()
 
         order = Order.objects.create(
             user=request.user,
             delivery_cost=delivery_cost,
             total_cost=total_amount + delivery_cost,
             delivery_address=user_address,
-            transaction=transaction,
+            status='PE'
         )
 
         for cart_item in cart_items:
@@ -51,8 +55,30 @@ class OrderViewSet(ModelViewSet):
                 price=cart_item.price,
             )
 
-        cart_items.delete()
-
         serializer = self.get_serializer(order)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=['post'])
+    @transaction.atomic
+    def process_payment(self, request, pk=None):
+        order = self.get_object()
+
+        success = True  # I simulated the payment gateway integration
+
+        if success:
+            Transaction.objects.create(
+                user=request.user,
+                order=order,
+                amount=order.total_cost,
+                status='PA',
+                payment_method="CD"
+            )
+
+            order.status = 'PC'
+            order.save()
+
+            ShoppingCart.objects.filter(user=request.user).delete()
+
+            return Response({"detail": "Payment successful"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": "Payment failed"}, status=status.HTTP_400_BAD_REQUEST)
