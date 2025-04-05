3c7681c8bc18130cc6003fe22a7529cb9ad11fb2
Augustine Baah Forson <forsonbaah@gmail.com>
2025-04-05 21:43:38 +0100

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,7 +37,7 @@ SECRET_KEY = env(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*.onrender.com"]
 
 
 # Application definition
